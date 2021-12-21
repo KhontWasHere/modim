e62ebd94de7963807210f5d7f047564bf28786e2
Khont Polisian <82388102+KhontWasHere@users.noreply.github.com>
2021-12-21 18:21:16 +0200

    Update say.js

--- commands/say.js
+++ commands/say.js
@@ -1,40 +1,44 @@
-const Discord = require("discord.js")
+const yedlixdiscord = require('discord.js');
 
-exports.run = async (client, message, args) => {
-   var tagdakiler = 0;
-  let tag = "☆";
-  message.guild.members.forEach(member => {
-    if(member.user.username.includes(tag)) {
-      tagdakiler = tagdakiler+1
-    }
-  })
- 
-  const voiceChannels = message.guild.channels.filter(c => c.type === 'voice');
-    let count = 0;
-   
-    for (const [id, voiceChannel] of voiceChannels) count += voiceChannel.members.size;
-      const emoji = client.emojis.find(emoji => emoji.name === "krkr"); // emojinin adını yazın.
-  const avezyembed = new Discord.RichEmbed()
-  .setThumbnail(client.user.avatarURL)
-  .setColor("black")
-  .addField(`${emoji} **Ses Kanallarında Bulunan Kişi Sayısı** ${emoji}`, `${count} **Kişi Bulunmaktadır.**`)  // emoji yazan yerlere yazdıgınız emoji gelecektir.
-.addField(`${emoji}  **Sunucuda Bulunan Kişi Sayısı** ${emoji} `,`${message.guild.memberCount} **Kişi Bulunmaktadır.**`)
-.addField(`${emoji} **Taglı Üye Sayısı** ${emoji}`,` ${tagdakiler}` +  ` **Kişi Tagımızda!**`)
-  .addField(`${emoji} **Sunucu Booster Sayısı** ${emoji}`,` 2` +  ` **Kişi Sunucuyu Boostlamış**`)
-        .setFooter(`İmmortal`)
-  message.channel.sendEmbed(avezyembed)
-  message.react()
-
-}
 exports.conf = {
   enabled: true,
-  guildOnly: false,
-  aliases: [],
-  permLevel: 0
+  guildOnly: true,
+  aliases: ["bilgi", "sunucubilgi", "sunucu-bilgi", "sb"],
+  permLevel: 0,
+  kategori: "bot",
 };
 
 exports.help = {
   name: 'say',
-  description: ' Sunucudaki ve sesdeki üyeleri gösterir',
-  usage: '!say'
-};;
+  description: 'yedlix',
+  usage: 'yedlix',
+
+};
+exports.run = async (client, message, args) => {
+    var tagdakiler = 0;
+  let tag = "☆";
+  message.guild.members.forEach(member => {
+    if(member.user.username.includes(tag)) {
+      tagdakiler = tagdakiler+1
+  const seskanallari = message.guild.channels.cache.filter(c => c.type === 'voice');
+  let yedlix3 = 0
+  let  yedlix2 = message.guild.members.cache.filter(m => !m.user.bot && m.user.presence.status !== "offline").size
+  let metinkanallari = message.guild.channels.cache.filter(m => m.type == "text").size;
+  for (const [id, voiceChannel] of seskanallari) yedlix3 += voiceChannel.members.size;
+  const yedlix = new yedlixdiscord.MessageEmbed()
+  .setColor("0x36393F")
+  .setTitle("İmmortal")
+  .setFooter("İmmortal")
+  .setTimestamp()
+  .setDescription(`
+ Toplam üye sayısı: **${message.guild.memberCount}**
+ Toplam çevrimiçi üye sayısı: **${yedlix2}**
+Taglı Üye Sayısı: ${tagdakiler}` +  ` **Kişi Tagımızda!**
+ Toplam metin kanalı sayısı: **${metinkanallari}**
+ Toplam ses kanalı sayısı: **${seskanallari.size}**
+ Toplam çevrimiçi durumda olan üye sayısı: **${message.guild.members.cache.filter(o => o.presence.status === 'online').size}**
+ Toplam boşta durumda olan üye sayısı: **${message.guild.members.cache.filter(i => i.presence.status === 'idle').size}**
+ Toplam rahatsız etme durumda olan üye sayısı: **${message.guild.members.cache.filter(i => i.presence.status === 'dnd').size}**
+`)
+  message.channel.send(yedlix)
+  }
